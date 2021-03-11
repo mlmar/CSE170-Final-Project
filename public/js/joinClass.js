503de12f6d3a4359b83e767de730c0cef984c3fe
mlmar <marcloussm@gmail.com>
2021-03-11 02:13:32 -0800

    dynamic search for class page to be consistent with members search

--- public/js/joinClass.js
+++ public/js/joinClass.js
@@ -1,2 +1,19 @@
 $(document).ready(function() {
+  $("#search").on("input", handleSearch);
 });
+
+function handleSearch() {
+  var val = $(this).val();
+
+  if(val.length) {
+    $.each($(".item"), function() {
+      if($(this).text().toLowerCase().includes(val.toLowerCase())) {
+        $(this).removeClass("hidden")
+      } else {
+        $(this).addClass("hidden");
+      }
+    });
+  } else {
+    $(".item").removeClass("hidden");
+  }
+}
