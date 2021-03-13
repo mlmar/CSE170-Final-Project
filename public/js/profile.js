30358b2821fa3085362b55891978a0915b350458
mlmar <marcloussm@gmail.com>
2021-03-13 02:55:51 -0800

    contact info

--- public/js/profile.js
+++ public/js/profile.js
@@ -7,8 +7,10 @@ function handleEdit() {
   $(this).addClass("hidden");
   $(".year-label").text("Graduating Year");
   $(".major-label").text("Major");
+  $(".phone-label").text("Phone Number");
   $(".edit-year").removeClass("hidden");
   $(".edit-major").removeClass("hidden");
+  $(".edit-phone").removeClass("hidden");
   $("#save-btn").removeClass("hidden");
   $("#sign-btn").addClass("hidden");
 }
@@ -16,15 +18,19 @@ function handleEdit() {
 function handleSave() {
   var year = $(".edit-year").val();
   var major = $(".edit-major").val();
+  var phone = $(".edit-phone").val();
   
-  $.post("/updateUser", {year, major}, function() {
+  $.post("/updateUser", {year, major, phone}, function() {
     $("#edit-btn").removeClass("hidden");
     $(".year-label").text(`Graduatng Year: ${year}`);
     $(".major-label").text(major);
+    $(".phone-label").text(phone);
     $(".edit-year").addClass("hidden");
     $(".edit-major").addClass("hidden");
+    $(".edit-phone").addClass("hidden");
     $(".edit-year").val(year);
     $(".edit-major").val(major);
+    $(".edit-phone").val(phone);
     $("#save-btn").addClass("hidden");
     $("#sign-btn").removeClass("hidden");
   })
--- routes/profile.js
+++ routes/profile.js
@@ -5,7 +5,8 @@ exports.view = function(req, res) {
     name: req.session.name,
     username: req.session.username,
     year: req.session.year,
-    major: req.session.major
+    major: req.session.major,
+    phone: req.session.phone
   })
 }
 
@@ -13,7 +14,9 @@ exports.update = function(req, res) {
   var user = req.session.username;
   users[user].year = req.body.year;
   users[user].major = req.body.major;
+  users[user].phone = req.body.phone;
   req.session.year = req.body.year;
   req.session.major = req.body.major;
+  req.session.phone = req.body.phone;
   res.send({ updated : true });
 }
